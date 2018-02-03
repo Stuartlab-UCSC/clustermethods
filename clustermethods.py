9eed5f673761fb7b9afaf568804c98b315834c01
Jeltje <jeltje.van.baren@gmail.com>
2018-02-03 04:18:17 +0000

    turned cluster results into object; created a holdout loop to decide which method results in most fragmentation

--- clustermethods.py
+++ clustermethods.py
@@ -25,6 +25,7 @@ import pandas as pd
 import robust
 import kmedoids
 import hdbscan
+import time
 
 from scipy.cluster.hierarchy import linkage, cut_tree
 from scipy.spatial.distance import squareform as sf
@@ -33,7 +34,14 @@ from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score, silhouette_score
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.cluster.bicluster import SpectralBiclustering
+import sklearn.preprocessing as pp
 from sklearn.preprocessing import QuantileTransformer, StandardScaler
+
+def passedTime(start, end):
+    hours, rem = divmod(end-start, 3600)
+    minutes, seconds = divmod(rem, 60)
+    ptime = "{:0>2}:{:0>2}:{:05.2f}".format(int(hours),int(minutes),seconds)
+    return ptime
 
 #####  get arguments  ###################################################
 parser = argparse.ArgumentParser(
@@ -47,6 +55,7 @@ group = parser.add_argument_group('required arguments')
 group.add_argument('inputfile', type=str, help='input tsv file')
 # optional flag
 parser.add_argument('--clusters', type=int, default=5,  help="Number of expected clusters (default 5)")
+parser.add_argument('--no_subsample', action='store_true',  help="Use the full set (Program uses 100 samples by default)")
 
 if len(sys.argv)==1:
     parser.print_help()
@@ -55,25 +64,28 @@ args = parser.parse_args()
 
 #####  Read and format input  ###########################################
 
+start = time.time()
 # gene by sample table
 # Pandas dataframes interpret header (df.columns.values or list(df)) and rownames (df.index)
 # this is harder to do with a direct numpy ndarray
-print >>sys.stderr, "reading", args.inputfile
+print >>sys.stderr, passedTime(start, time.time()), "reading", args.inputfile
 df=pd.read_table(args.inputfile, sep='\t', header=0, index_col=0)
 
-print >>sys.stderr, "filtering and normalizing samples..."
+print >>sys.stderr, passedTime(start, time.time()),  "filtering and normalizing samples..."
 # randomly select 100 samples (comment this out if you want to do a full scale run)
-ids = list(df)
-np.random.shuffle(ids)
-df = df.loc[:,ids[:100]]
+if not args.no_subsample:
+    ids = list(df)
+    np.random.shuffle(ids)
+    df = df.loc[:,ids[:100]]
 
 # remove genes with average expression of 1 fpkm or less (this removes ~45000 genes, keeping 15,000)
 df = df[df.mean(1)>1]
+print >>sys.stderr, df.shape
 
 # (quantile) normalize - it actually makes no difference for finding the most variable genes
 #qt = StandardScaler(copy=False)  # this is another method
 qt = QuantileTransformer(random_state=0, copy=False)
-qt.fit_transform(df.transpose())
+#qt.fit_transform(df.transpose())
 
 # Verena's method works too:
 #for col in list(df):
@@ -94,106 +106,150 @@ sbg = df.transpose()
 
 
 #####  Cluster  ######################################################
+methods = []
 
 # keep track of cluster assignments and silhouette scores
 labels = dict()
 silscore = dict()
 
-print >>sys.stderr, "Clustering..."
+print >>sys.stderr, passedTime(start, time.time()),  "Clustering..."
 
+print >>sys.stderr, passedTime(start, time.time()),  "HDBSCAN"
 ## HDBSCAN
 # this method allows samples to not be assigned to a cluster at all
 # I can't really make it work yet with anything > min_cluster_size=2, even though that
 # does output more than 2 clusters.
 clusterer = hdbscan.HDBSCAN(min_cluster_size=2)
-labels['hdbscan'] = clusterer.fit_predict(sbg)
-# remove unclustered samples from array
-keepme = [i for i, j in enumerate(labels['hdbscan'].tolist()) if j != -1]
-reduce=sbg.ix[keepme]
-silscore['hdbscan'] = silhouette_score(reduce, labels['hdbscan'][keepme])
+labels = clusterer.fit_predict(sbg)
 
+# remove unclustered samples from array
+keepme = [i for i, j in enumerate(labels.tolist()) if j != -1]
+reduce=sbg.ix[keepme]
+# how many clusters?
+print "HBSCAN cluster count",len(set(labels))
+silscore = silhouette_score(reduce, labels[keepme])
+hdbObject = robust.cmethod('hdbscan', labels, silscore, args.clusters)
+methods.append(hdbObject)
+
+print >>sys.stderr, passedTime(start, time.time()),  "Bicluster"
 ## BICLUSTERING
 # This clusters samples and genes at the same time, supposedly allowing less influence
 # of genes with really similar expresson patterns. I haven't played with the parameters yet
 n_clusters = (args.clusters, 5)
 biclust = SpectralBiclustering(n_clusters=n_clusters, method='log', random_state=0)
 biclust.fit(sbg)
-labels['biclust']=biclust.row_labels_
-silscore['biclust'] = silhouette_score(sbg, labels['biclust'])
+labels = biclust.row_labels_
+silscore = silhouette_score(sbg, labels)
+biclustObject = robust.cmethod('biclust', labels, silscore, args.clusters)
+methods.append(biclustObject)
 
+print >>sys.stderr, passedTime(start, time.time()),  "Hier Pearson Complete"
 ## HIERARCHICAL CLUSTERING, PEARSON, COMPLETE LINKAGE
 # depends on a distance matrix, which can be created in a variety of ways. Have not tried others yet
 s_distance = pairwise_distances(sbg, metric='correlation')
 hier_clust_samples =linkage(sf(s_distance), 'complete')
 sample_groups = cut_tree(hier_clust_samples, n_clusters=args.clusters)
-labels['hier_pearson_complete'] = sample_groups.flatten()
-silscore['hier_pearson_complete'] = silhouette_score(s_distance, labels['hier_pearson_complete'], metric="precomputed")
+labels = sample_groups.flatten()
+silscore = silhouette_score(s_distance, labels, metric="precomputed")
+hierpcObject = robust.cmethod('hier_pearson_complete', labels, silscore, args.clusters)
+methods.append(hierpcObject)
 
+print >>sys.stderr, passedTime(start, time.time()),  "Hier Pearson Average"
 ## HIERARCHICAL CLUSTERING, PEARSON, AVERAGE LINKAGE
 avg_samples =linkage(sf(s_distance), 'average')
-avg_sample_groups = cut_tree(avg_samples, n_clusters=args.clusters)
-labels['hier_pearson_avg'] = avg_sample_groups.flatten()
-silscore['hier_pearson_avg'] = silhouette_score(s_distance, labels['hier_pearson_avg'], metric="precomputed")
+sample_groups = cut_tree(avg_samples, n_clusters=args.clusters)
+labels = sample_groups.flatten()
+silscore = silhouette_score(s_distance, labels, metric="precomputed")
+hierpaObject = robust.cmethod('hier_pearson_average', labels, silscore, args.clusters)
+methods.append(hierpaObject)
 
+print >>sys.stderr, passedTime(start, time.time()),  "Hier Spearman Average"
 ## HIERARCHICAL CLUSTERING, SPEARMAN, AVERAGE LINKAGE
 # Rank based clustering seems to work a bit better according to Jaskowiak paper and Max Hauessler.
 spearmat, pval = spearmanr(sbg, axis=1)
 spearmat = np.round(1-spearmat, 6) # without rounding there are non-zero values on the diagonals
 spearman_samples =linkage(sf(spearmat), 'average')
-spearman_sample_groups = cut_tree(spearman_samples, n_clusters=args.clusters)
-labels['hier_spearman_avg'] = spearman_sample_groups.flatten()
-silscore['hier_spearman_avg'] = silhouette_score(spearmat, labels['hier_spearman_avg'], metric="precomputed")
+sample_groups = cut_tree(spearman_samples, n_clusters=args.clusters)
+labels = sample_groups.flatten()
+silscore = silhouette_score(spearmat, labels, metric="precomputed")
+hiersaObject = robust.cmethod('hier_spearman_average', labels, silscore, args.clusters)
+methods.append(hiersaObject)
 
+print >>sys.stderr, passedTime(start, time.time()),  "KMEANS"
 ## KMEANS
 # This is probabilistic and doesn't give the same outcome twice.
 # I thought max_iter was supposed to result in convergence but it doesn't seem to help much
-kmeans = KMeans(init='k-means++', n_clusters=args.clusters, n_init=args.clusters, max_iter=100)
-labels['kmeans'] = kmeans.fit_predict(sbg)
-silscore['kmeans'] = silhouette_score(sbg, labels['kmeans'])
+# run 10 times, keep the one with best silhouette score
+bestsil = 0
+for i in xrange(100):
+    kmeans = KMeans(init='k-means++', n_clusters=args.clusters, n_init=args.clusters, max_iter=100)
+    labels = kmeans.fit_predict(sbg)
+    silscore = silhouette_score(sbg, labels)
+    if silscore > bestsil:
+        kmeansObject = robust.cmethod('kmeans', labels, silscore, args.clusters)
+        bestsil = silscore
+methods.append(kmeansObject)
 
+print >>sys.stderr, passedTime(start, time.time()),  "KMEDOIDS"
 ## KMEDOIDS
 # Same issue as Kmeans. It is supposed to be better for RNASeq than Kmeans but I haven't seen 
 # much to convince me
-medoids, clusterinfo, labels['kmed'] = kmedoids.kMedoids(s_distance, args.clusters)
-silscore['kmed'] = silhouette_score(s_distance, labels['kmed'])
+# run 10 times, keep the one with best silhouette score
+bestsil = 0
+for i in xrange(100):
+    medoids, clusterinfo, labels = kmedoids.kMedoids(s_distance, args.clusters)
+    silscore = silhouette_score(s_distance, labels)
+    if silscore > bestsil:
+        kmedObject = robust.cmethod('kmedoids', labels, silscore, args.clusters)
+        bestsil = silscore
+methods.append(kmedObject)
 
-
-#####  Remove skewed results  ######################################################
-
-# If the vast majority of samples (currently 80%) end up in the same cluster, something's wrong.
-rmlist = []
-for id, l in labels.iteritems():
-    retval = id
-    for c in set(l):
-        ct = list(l).count(c)
-        if (float(ct)/len(l)) > 0.8:
-            print "{} cluster {} occurs {} times, skipping this method".format(id, str(c), ct)
-            rmlist.append(id)
-            break
-
-for id in rmlist:
-    del(silscore[id])
-    del(labels[id])
 
 #####  Validate results  ######################################################
 
-for method, score in silscore.iteritems():
-    print "{} silhouette score for {}".format(score, method)
+for i in methods:
+    if i.ok:
+        print "{0:.2f} silhouette score for {1}".format(i.silscore, i.name)
 
-# Methods often agree on core groups of samples. This part of the code is not robust, it currently
-# depends on commenting out lines to see the effects of removing one or more of the methods.
-# it will also die horribly if the skew filter removed the labels dictionary entry for the method.
-setlist = []
-#setlist.append(robust.list_duplicates(labels['kmeans']))
-#setlist.append(robust.list_duplicates(labels['kmed']))
-setlist.append(robust.list_duplicates(labels['hier_spearman_avg']))
-setlist.append(robust.list_duplicates(labels['hier_pearson_avg']))
-setlist.append(robust.list_duplicates(labels['hier_pearson_complete']))
-#setlist.append(robust.list_duplicates(labels['hdbscan']))
-setlist.append(robust.list_duplicates(labels['biclust']))
+
+minsize = 3
+print >>sys.stderr, passedTime(start, time.time()),  "Finding consistent groups"
+setlist = [i.dups for i in methods if i.ok]
 
 # print consistent groups of samples
-robust.persistent_groups(copy.copy(setlist), list(sbg.index))
+grouplist1 = robust.persistent_groups(copy.copy(setlist), list(sbg.index), minsize)
+for i in xrange(min(args.clusters, len(grouplist1))):
+    print "Group {}, size {}: {}".format(i, len(grouplist1[i])," ".join(str(x) for x in grouplist1[i]))
+
+
+####  Hold out #####################################################################
+# if we hold out one method in the group calculation, do we get larger groups?
+# not sure how to do this other than brute force it
+
+#membercount = dict()
+bestgroup = grouplist1
+maxsize = 0
+namelist = [i.name for i in methods if i.ok]
+for i in xrange(len(namelist)):
+    newset = setlist[::]
+    del(newset[i])
+    count = 0
+#    membercount[namelist[i]] = 0
+    grouplist = robust.persistent_groups(newset[::], list(sbg.index), minsize)
+    # count size of first groups
+    for x in xrange(min(len(grouplist), args.clusters)):
+         count += len(grouplist[x])
+    # allow a bit of wiggle
+    if count > maxsize +5:
+        print "best group currently the one without {}, groupsize is then {}".format(namelist[i], count)
+        bestgroup = grouplist
+        maxsize = count
+    print i, namelist[i], count
+
+for i in xrange(min(args.clusters, len(grouplist1))):
+    print "Group {}, size {}: {}".format(i, len(bestgroup[i])," ".join(str(x) for x in bestgroup[i]))
+
+
 
 
 
--- robust.py
+++ robust.py
@@ -1,3 +1,4 @@
+import sys
 """
 This set of functions turns a list of repeating numbers (such as cluster assignments) into sets of  
 positions that always co-occur.
@@ -9,15 +10,22 @@ from collections import defaultdict
 from sklearn.metrics import adjusted_rand_score
 import copy
 
-def persistent_groups(setlist, samples):
-    """Find samples that always group together"""
+def persistent_groups(setlist, samples, minsize):
+    """Find samples that always group together in groups of minsize"""
     tally = common_idx(setlist.pop(0), setlist.pop(0))
     while(setlist):
         tally = common_idx(tally, setlist.pop(0))
     #Match sample IDs to their indices; each set in setlist is a group
+    retlist = []
     for myset in tally:
-        dada = sorted([samples[i] for i in myset])
-        print "Group:",len(dada)," ".join(str(x) for x in dada)
+        slist = sorted([samples[i] for i in myset])
+        if len(slist) < minsize:
+            continue
+	retlist.append(slist)
+    # sort so that the largest groups are listed first
+    retlist.sort(key=len, reverse=True)
+    return retlist
+
 
 def list_duplicates(mylist):
     """Identify sets of positions with the same value"""
@@ -49,4 +57,20 @@ def adj_rand_score(labeldict):
         for b_label in dictcopy:
             print "{0:0.2f}\t{1} vs {2}".format(adjusted_rand_score(labeldict[a_label], dictcopy[b_label]), a_label, b_label)
 
+class cmethod():
+    def __init__(self, name, labels, silscore, fract):
+        self.name = name
+        self.silscore = silscore
+	self.labels = labels
+        self.dups = list_duplicates(self.labels)
+        self.skewCheck(fract)
+    def skewCheck(self, fract):
+        self.ok = True
+        # If more than <fract> fraction of samples end up in the same cluster, something's wrong.
+        for c in set(self.labels):
+            ct = list(self.labels).count(c)
+            if (float(ct)/len(self.labels)) > fract:
+                print >>sys.stderr, "{} cluster {} occurs {} times, skipping this method".format(self.name, str(c), ct)
+                self.ok = False
+                break
 
